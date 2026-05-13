8ba0e5f65218594f00026cfe834aeb5f640a3cbd
Santhosh173 <161047241+santhosh-v-173@users.noreply.github.com>
2026-05-13 14:31:50 +0000

    Mississippi task

--- mississippi.py
+++ mississippi.py
@@ -1,10 +1,14 @@
-word = "Mississippi"
-result = {}
+def most_frequent(word):
+    result = {}
 
-for i in word:
-    result[i] = word.count(i)
+    for i in word:
+        result[i] = word.count(i)
 
-sorted_result = sorted(result.items(),key=lambda x: x[1], reverse=True)
+    sorted_result = sorted(result.items(),key=lambda x: x[1], reverse=True)
+    return sorted_result
+    
 
-for key,value in sorted_result:
-    print(key+" = 0"+str(value))
+result = most_frequent("Mississippi")
+
+for key,value in result:
+        print(key+" = 0" +str(value))
